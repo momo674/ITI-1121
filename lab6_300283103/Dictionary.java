15410a704b2a67ac8ee8a157027786aed8773d45
Mohamad Radaideh <117695265+momo674@users.noreply.github.com>
2023-03-02 23:02:17 -0500

    full mark

--- lab6_300283103/Dictionary.java
+++ lab6_300283103/Dictionary.java
@@ -15,66 +15,88 @@ public class Dictionary implements Map<String, Integer> {
     }
 
     public Dictionary() {
-        this.elems = new Pair[10];
-		this.count = 0;
+        this.elems = new Pair[INITIAL_CAPACITY];
+        this.count = 0;
     }
 
     @Override
     public void put(String key, Integer value) {
-        if (key != null && value != null){
-			if (this.count > this.elems.length -1 ) {this.increaseCapacity();}
-			this.elems[this.count] = new Pair(key, value);
-			this.count++;
-		}
+        if (key == null || value == null) {return;}
+        else{
+            if (this.count == this.elems.length) {this.increaseCapacity();}
+            this.elems[this.count] = new Pair(key,value);
+            this.count++;
+        }
     }
 
     private void increaseCapacity() {
-		Pair[] larger_elems = new Pair[this.elems.length + INCREMENT];
-			for (int i = 0; i < this.elems.length; i ++) {
-				larger_elems[i] = this.elems[i];
-			}
-			this.elems = larger_elems;
-        /* Your code here.  Use this in put() where necessary. */
+        Pair[] new_elems = new Pair[this.elems.length + INCREMENT];
+        int new_count = 0;
+        for (int i = 0; i < this.elems.length; i++){
+            new_elems[i] = this.elems[i];
+            new_count++;
+        }
+        this.elems = new_elems;
+        this.count = new_count;
     }
 
     @Override
     public boolean contains(String key) {
-        if (key == null) {return false;}
-		for (int i = 0; i < this.elems.length; i++) {
-			if (this.elems[i].getKey().equals(key)) {return true;}
-		}
-		return false;
+        if (key == null) { return false;}
+        for (int i = 0; i < this.elems.length; i++) {
+            if (this.elems[this.elems.length - 1 - i] != null){
+                if (this.elems[this.elems.length - 1 - i].getKey().equals(key)) {
+                    return true;
+                }
+            }
+            
+        }
+        return false;  
     }
 
     @Override
     public Integer get(String key) {
-		if (!this.contains(key)) {return null;}
-		for (int i = this.count; i >= 0; i--) {
-			if (this.elems[i].getKey().equals(key)) {return this.elems[i].getValue();}
-		}
-		return null;
-		
-        /* Your code here. */
+        if (key == null) { return -1;}
+        for (int i = 0; i < this.elems.length; i++) {
+            if (this.elems[this.elems.length - 1 - i] != null){
+                if (this.elems[this.elems.length - 1 - i].getKey().equals(key)) {
+                    return (this.elems[this.elems.length - 1 - i].getValue());
+                }
+            }
+            
+        }
+        return -1;
     }
 
     @Override
     public void replace(String key, Integer value) {
-        for (int i = this.count; i >= 0; i--) {
-			if (this.elems[i].getKey().equals(key)) {this.elems[i].setValue(value);}
-		}
+        if (key == null) { return;}
+        for (int i = 0; i < this.elems.length; i++) {
+            if (this.elems[this.elems.length - 1 - i] != null){
+                if (this.elems[this.elems.length - 1 - i].getKey().equals(key)) {
+                    this.elems[this.elems.length - 1 - i].setValue(value);
+                }
+            }
+            
+        }
+        return;
     }
 
     @Override
     public Integer remove(String key) {
-      for (int i = this.count; i >= 0; i--){
-        if (this.elems[i].getKey().equals(key)) {
-			    Integer value = this.elems[i].getValue();
-			    this.elems[this.count] = null;
-			    this.count --;
-          return value;
-			  }
-      }
-			return null;
+        if (key == null) { return -1;}
+        for (int i = 0; i < this.elems.length; i++) {
+            if (this.elems[this.elems.length - 1 - i] != null){
+                if (this.elems[this.elems.length - 1 - i].getKey().equals(key)) {
+                    Integer desired= (this.elems[this.elems.length - 1 - i].getValue());
+                    this.elems[this.elems.length - 1 - i] = null;
+                    this.count--;
+                    return desired;
+                }
+            }
+            
+        }
+        return -1;
     }
 
     @Override
---
+++ lab6_300283103/README.txt.txt
@@ -0,0 +1,7 @@
+Student name: Mohamad Radaideh
+Student number: 300283103
+Course code: ITI1121
+Lab section: A-4
+
+This archive contains the 7 files of lab 6, that is, this file (README.txt),
+plus ArrayStack.java, Dictionary.java, DynamicArrayStack.java, Pair.java, Map.java, Stack.java.
