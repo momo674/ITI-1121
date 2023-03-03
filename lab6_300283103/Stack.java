bcd36aa0da28e7a1fdc98679967282c30e5fb8bc
Mohamad Radaideh <117695265+momo674@users.noreply.github.com>
2023-03-02 23:02:36 -0500

    Delete lab6_300283103 directory

--- lab6_300283103/Stack.java
+++
@@ -1,8 +0,0 @@
-public interface Stack<E> {
-
-    public abstract boolean isEmpty();
-    public abstract E peek();
-    public abstract E pop();
-    public abstract void push( E element);
-    public abstract void clear();
-}
